a344e57ed5bc1f0afbbc65fad56d3313ff244efb
romuloabbiati <rhabbiati@hotmail.com>
2023-01-26 07:29:25 +0000

    Save category and findById category

--- src/main/java/com/smartgroup/smartmoney/resource/CategoryResource.java
+++ src/main/java/com/smartgroup/smartmoney/resource/CategoryResource.java
@@ -1,11 +1,19 @@
 package com.smartgroup.smartmoney.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.smartgroup.smartmoney.model.Category;
 import com.smartgroup.smartmoney.repository.CategoryRepository;
@@ -20,6 +28,32 @@ public class CategoryResource {
 	@GetMapping
 	public List<Category> findAll() {
 		return categoryRepository.findAll();
+	}
+	
+//	@GetMapping(path = "/{id}")
+//	public ResponseEntity<Category> findById(@PathVariable Long id) {
+//		Optional<Category> category = categoryRepository.findById(id);
+//		
+//		return category.isPresent() ? ResponseEntity.ok(category.get()) : ResponseEntity.notFound().build();
+//	}
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		return categoryRepository.findById(id)
+			.map(category -> ResponseEntity.ok(category))
+			.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> save(@RequestBody Category category, HttpServletResponse response) {
+		Category savedCategory = categoryRepository.save(category);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+			.buildAndExpand(savedCategory.getId()).toUri();
+		
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(savedCategory);
 	}
 	
 }
