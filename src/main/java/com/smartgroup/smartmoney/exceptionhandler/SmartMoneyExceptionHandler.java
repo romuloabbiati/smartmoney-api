20ea1843b86a178c3b81c159fe803fa87aa08a66
romuloabbiati <rhabbiati@hotmail.com>
2023-01-31 06:54:28 +0000

    DataIntegrityViolationException handler

--- src/main/java/com/smartgroup/smartmoney/exceptionhandler/SmartMoneyExceptionHandler.java
+++ src/main/java/com/smartgroup/smartmoney/exceptionhandler/SmartMoneyExceptionHandler.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -61,6 +63,14 @@ public class SmartMoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		List<Error> errors = Arrays.asList(new Error(userMessage, devMessage));
 		
 		return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+	
+	@ExceptionHandler({ DataIntegrityViolationException.class })
+	public ResponseEntity<Object> handleDataIntegrityViolation(DataIntegrityViolationException ex, WebRequest request) {
+		String userMessage = messageSource.getMessage("resource.cannot-process-request", null, Locale.getDefault());
+		String devMessage = ExceptionUtils.getRootCauseMessage(ex);
+		List<Error> errors = Arrays.asList(new Error(userMessage, devMessage));
+		return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
 	private List<Error> createErrorList(BindingResult bindingResult) {
--- src/main/java/com/smartgroup/smartmoney/resource/BillResource.java
+++ src/main/java/com/smartgroup/smartmoney/resource/BillResource.java
@@ -3,13 +3,21 @@ package com.smartgroup.smartmoney.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.smartgroup.smartmoney.event.ResourceCreatedEvent;
 import com.smartgroup.smartmoney.model.Bill;
 import com.smartgroup.smartmoney.repository.BillRepository;
 
@@ -20,6 +28,9 @@ public class BillResource {
 	@Autowired
 	private BillRepository billRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Bill> findAll() {
 		return billRepository.findAll();
@@ -29,6 +40,13 @@ public class BillResource {
 	public ResponseEntity<Bill> findById(@PathVariable Long id) {
 		Optional<Bill> bill = billRepository.findById(id);
 		return bill.isPresent() ? ResponseEntity.ok(bill.get()) : ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Bill> save(@Valid @RequestBody Bill bill, HttpServletResponse response) {
+		Bill savedBill = billRepository.save(bill);
+		publisher.publishEvent(new ResourceCreatedEvent(this, response, savedBill.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedBill);
 	}
 	
 }
