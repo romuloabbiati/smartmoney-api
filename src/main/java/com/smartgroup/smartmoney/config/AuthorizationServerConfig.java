0a7919a1747ff4f9991817f75e83ab63f4a1090d
romuloabbiati <rhabbiati@hotmail.com>
2023-02-06 05:08:57 +0000

    jwt config

--- src/main/java/com/smartgroup/smartmoney/config/AuthorizationServerConfig.java
+++ src/main/java/com/smartgroup/smartmoney/config/AuthorizationServerConfig.java
@@ -4,14 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -22,6 +23,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -35,25 +39,37 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	            .withClient("mobile")
 	            .secret(passwordEncoder.encode("m0b1l30")) // m0b1l30
 	            .scopes("read")
-	            .authorizedGrantTypes("password")
-	            .accessTokenValiditySeconds(1800);
+	            .authorizedGrantTypes("password", "refresh_token")
+	            .accessTokenValiditySeconds(20)
+	            .refreshTokenValiditySeconds(3600 * 24);
 	}
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
+			.authenticationManager(authenticationManager)
+			.accessTokenConverter(accessTokenConverter())
 			.tokenStore(tokenStore())
-			.authenticationManager(authenticationManager);
-	}
-	
-	@Override
-	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-	    security.checkTokenAccess("permitAll()");
+			.userDetailsService(userDetailsService)
+			.reuseRefreshTokens(false);
 	}
 	
 	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("3032885ba9cd6621bcc4e7d6b6c35c2b");
+		return accessTokenConverter;
+	}
+
+	// method for opaque token
+//	@Override
+//	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+//	    security.checkTokenAccess("permitAll()");
+//	}
+	
+	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(accessTokenConverter());
 	}
 
 }
