991ee4c468f2df8e755231abafa01d12c25ec9f5
romuloabbiati <rhabbiati@hotmail.com>
2023-02-07 06:52:23 +0000

    config to add refresh token to cookie

--- src/main/java/com/smartgroup/smartmoney/config/AuthorizationServerConfig.java
+++ src/main/java/com/smartgroup/smartmoney/config/AuthorizationServerConfig.java
@@ -33,8 +33,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	            .withClient("angular")
 	            .secret(passwordEncoder.encode("@ngul@r0")) // @ngul@r0
 	            .scopes("read", "write")
-	            .authorizedGrantTypes("password")
-	            .accessTokenValiditySeconds(1800)
+	            .authorizedGrantTypes("password", "refresh_token")
+	            .accessTokenValiditySeconds(20)
+	            .refreshTokenValiditySeconds(3600 * 24)
 	        .and()
 	            .withClient("mobile")
 	            .secret(passwordEncoder.encode("m0b1l30")) // m0b1l30
